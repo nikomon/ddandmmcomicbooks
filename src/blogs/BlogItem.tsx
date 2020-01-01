d08798d67e9998150421d8ef02a0527c86cc9ff1
Niko Niittyranta <niko.niittyranta@aller.com>
2020-01-02 00:21:45 +0200

    Add: id as key

--- src/blogs/BlogItem.tsx
+++ src/blogs/BlogItem.tsx
@@ -2,13 +2,13 @@ import React, { useContext } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { navigationContext } from '../services/navigationContext';
  
-export const BlogItem = (props: any) => {
+export const BlogItem = (props: any) => {  
   const navigationItems = useContext(navigationContext);
   const blogItem = navigationItems.find(item => item.slug === `/${props.match.params.slug}`);
   
   if(blogItem) {
     const { body, id } = blogItem;
-    return <ReactMarkdown source={body}/>
+    return <ReactMarkdown key={id} source={body}/>
   }
 
   return (<div>Page not found</div>);
